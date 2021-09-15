97b8a93fbe59a222973dc6e3b1c83156535c6a9d
lelikone <2257855@gmail.com>
2021-09-15 22:30:16 +0300

    13 урок условия без практики

--- js/script.js
+++ js/script.js
@@ -1,21 +1,39 @@
 "use strict";
 
-const numberOfFilms = +prompt("Сколько фильмов", "");
+// const numberOfFilms = +prompt("Сколько фильмов", "");
+//
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false,
+// };
+//
+// const a = prompt("Один из последних просмотренных фильмов?", ""),
+//   b = prompt("На сколько оцените его?", ""),
+//   c = prompt("Один из последних просмотренных фильмов?", ""),
+//   d = prompt("На сколько оцените его?", "");
+//
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+//
+// console.log(personalMovieDB);
 
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
-};
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-  b = prompt("На сколько оцените его?", ""),
-  c = prompt("Один из последних просмотренных фильмов?", ""),
-  d = prompt("На сколько оцените его?", "");
+const num = 50;
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
+switch (num) {
+  case 49:
+    console.log('неверно');
+    break;
+  case 100:
+    console.log('неверно');
+    break;
+  case 51:
+    console.log('верно!!');
+    break;
+  default:
+    console.log('не в этот раз');
+    break;
+}
