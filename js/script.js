e5ae87fea2126a71aa380d79f1d80157d616d4d6
lelikone <2257855@gmail.com>
2021-09-16 00:54:36 +0300

    18. Практика , ч3. Используем функции

--- js/script.js
+++ js/script.js
@@ -1,21 +1,63 @@
 "use strict";
 
-const numberOfFilms = +prompt("Сколько фильмов", "");
-
 const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
+    count: start(),
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-  b = prompt("На сколько оцените его?", ""),
-  c = prompt("Один из последних просмотренных фильмов?", ""),
-  d = prompt("На сколько оцените его?", "");
+function start() {
+    let numberOfFilms = +prompt("Сколько фильмов вы посмотрели", "");
+    while ( numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms) ) {
+        numberOfFilms = +prompt("Сколько фильмов вы посмотрели", "");
+    }
+    return numberOfFilms;
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt("Один из последних просмотренных фильмов?", ""),
+            b = prompt("На сколько оцените его?", "");
 
-console.log(personalMovieDB);
+        if (a != null && b != null && a != '' && b != '' && a.length < 50)  {
+            personalMovieDB.movies[a] = b;
+        } else {
+            i--;
+        }
+    }
+}
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('нормально фильмов');
+    }  else if (personalMovieDB.count >= 30) {
+        console.log('вы киноман');
+    } else {
+        console.log('произошла ошибка');
+    }
+    return personalMovieDB.count;
+}
+detectPersonalLevel();
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`ваш любимый жанр ${i}`);
+    }
+    return personalMovieDB.genres;
+}
+writeYourGenres();
+
+function showMyDB() {
+    if (personalMovieDB.privat === false) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB();
+
+
+
