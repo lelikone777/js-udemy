6f01ea78c9359a89a7ac6da13cdff9e2f40ee41f
lelikone <2257855@gmail.com>
2021-09-16 17:23:51 +0300

    21. Массивы и псевдомассивы

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
 "use strict";
+
+const arr = [12,2,31,4,15];
+arr.sort(compareNum);
+console.log(arr);
+
+function compareNum(a,b) {
+    return a - b;
+}
+
+const str = prompt('', '');
+const products = str.split(', ');
+products.sort();
+console.log(products.join('; '));
+
+
+// arr[99] = 0;
+// console.log(arr);
+// console.log(arr.length);
+
+// arr.forEach(function(item,i, arr) {
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
+
+
+
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//     console.log(value);
+// }
