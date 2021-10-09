c3930f32902c111ef45d36fb32ef526d50892561
alex <2257855@gmail.com>
2021-10-09 15:18:34 +0300

    endlesson 32

--- preview/script.js
+++ preview/script.js
@@ -1,25 +1,12 @@
-const btns = document.querySelectorAll('button'),
-    overlay = document.querySelector('.overlay');
-let i = 0;
+// console.log(document.body.firstChild);
+// console.log(document.body.childNodes);
 
-const deleteElement = (e) => {
-    console.log(e.target);
-    console.log(e.type);
-    // i++;
-    // if (i === 1) {
-    //     btn.removeEventListener('click', deleteElement);
-    // }
-};
+// console.log(document.querySelector('#current').parentElement.parentElement);
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
 
-btns.forEach(btn => {
-    btn.addEventListener('click', deleteElement, {once: true});
-});
-
-// btn.addEventListener('click', deleteElement);
-// overlay.addEventListener('click', deleteElement);
-
-const link = document.querySelector('a');
-link.addEventListener('click', function (event) {
-    event.preventDefault();
-    console.log(event.target);
-});
+for (let node of document.body.childNodes) {
+    if (node.nodeName === '#text') {
+        continue;
+    }
+    console.log(node);
+}
