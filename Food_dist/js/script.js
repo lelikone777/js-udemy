1c8ea6a804de32ea564dfe1c30cbc30fc5ee79e8
Kalinin Alexey <akalinin@usetech.ru>
2021-10-26 14:03:45 +0300

    41 process timer

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -39,15 +39,43 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 
   //Timer
-
-  const deadline = '3030-05-11';
+  const deadline = "2020-05-20";
 
   function getTimeRemaining(endtime) {
-    const t = Date.parse(endtime) - Date.parse(new Date()),
-    days = Math.floor();
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    const days = Math.floor(t / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((t / 1000 / 60) % 60);
+    const seconds = Math.floor((t / 1000) % 60);
+
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
   }
 
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector("#days"),
+      hours = timer.querySelector("#hours"),
+      minutes = timer.querySelector("#minutes"),
+      seconds = timer.querySelector("#seconds"),
+      timeInterval = setInterval(updateClock, 1000);
 
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+      days.innerHTML = t.days;
+      hours.innerHTML = t.hours;
+      minutes.innerHTML = t.minutes;
+      seconds.innerHTML = t.seconds;
 
-
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+  setClock(".timer", deadline);
 });
