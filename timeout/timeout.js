97bc851901cc661ed3d1a7e9975b520a2409c984
KALININ ALEXEY <56075795+lelikone777@users.noreply.github.com>
2021-10-26 07:05:58 +0300

    39 end

--- timeout/timeout.js
+++ timeout/timeout.js
@@ -1,5 +1,32 @@
-const timerId = setTimeout(logger, 2000, "Hello123");
-clearInterval(timerId);
-function logger() {
-  console.log("text");
+// const timerId = setTimeout(logger, 2000, "Hello123");
+// clearInterval(timerId);
+// function logger() {
+//   console.log("text");
+// }
+// let id = setTimeout(function log() {
+//   console.log('hello');
+//   id = setTimeout(log, 500);
+// },500);
+
+
+const btn = document.querySelector('.btn');
+let timerId,
+    i = 0;
+
+function myAnimation() {
+  const elem = document.querySelector('.box');
+  let pos = 0;
+
+const id = setInterval(frame, 10);
+function frame(){
+  if  (pos === 300) {
+    clearInterval();
+  } else {
+    pos++;
+    elem.style.top = pos + 'px';
+    elem.style.left = pos + 'px';
+  }
 }
+}
+
+btn.addEventListener('click', myAnimation);
