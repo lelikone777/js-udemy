9259c5861d322f81ae8255c5774921f0ef8ea1ff
Kalinin Alexey <akalinin@usetech.ru>
2021-10-31 16:04:37 +0300

    55 end

--- 55. Promise (ES6)/55. Promise (ES6).js
+++ 55. Promise (ES6)/55. Promise (ES6).js
@@ -2,23 +2,54 @@
 
 console.log("Запрос данных...");
 
-const req = new Promice(function (resolve, reject) {
-  setTimeout(() => {
-    console.log("Подготовка данных...");
-    const product = {
-      name: "TV",
-      price: "2000",
-    };
+// const req = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     console.log("Подготовка данных...");
+//     const product = {
+//       name: "TV",
+//       price: "2000",
+//     };
+//
+//     resolve(product);
+//   }, 2000);
+// });
+//
+// req
+//   .then((product) => {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         product.status = "order";
+//         resolve(product);
+//       }, 2000);
+//     });
+//   })
+//   .then((data) => {
+//     data.modify = true;
+//     return data;
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch(() => {
+//     console.error("Произошла ошибка...");
+//   })
+//   .finally(() => {
+//     console.log("finally...");
+//   });
 
-    resolve();
-  }, 2000);
+const test = (time) => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(), time);
+  });
+};
+
+// test(1000).then(() => console.log("1000"));
+// test(1000).then(() => console.log("2000"));
+
+// Promise.all([test(1000), test(2000)]).then(() => {
+//   console.log("all");
+// });
+
+Promise.race([test(1000), test(2000)]).then(() => {
+  console.log("race");
 });
-
-req.then(() => {
-  console.log("Данные получены");
-});
-
-// setTimeout(() => {
-//   product.status = "order";
-//   console.log(product);
-// }, 2000);
