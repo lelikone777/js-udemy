3118f6d96c873baa00d150ac7e615b74f27ab04f
Kalinin Alexey <akalinin@usetech.ru>
2021-10-31 15:41:02 +0300

    55

--- 55. Promise (ES6)/55. Promise (ES6).js
+++ 55. Promise (ES6)/55. Promise (ES6).js
@@ -2,4 +2,23 @@
 
 console.log("Запрос данных...");
 
-setTimeout(() => {});
+const req = new Promice(function (resolve, reject) {
+  setTimeout(() => {
+    console.log("Подготовка данных...");
+    const product = {
+      name: "TV",
+      price: "2000",
+    };
+
+    resolve();
+  }, 2000);
+});
+
+req.then(() => {
+  console.log("Данные получены");
+});
+
+// setTimeout(() => {
+//   product.status = "order";
+//   console.log(product);
+// }, 2000);
